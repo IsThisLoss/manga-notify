8322da339c7914a0b9a27e365cafaac1ee5859a4
isthisloss <kopturovdim@gmail.com>
2022-08-20 10:53:03 +0000

    fix docker

--- manga_notify/settings.py
+++ manga_notify/settings.py
@@ -5,8 +5,6 @@ import pydantic
 
 class Settings(pydantic.BaseSettings):
     tg_token: str                       # telegram bot token
-    db_string: str = '/data/db.sqlite'  # path to sqlite database
-    db_init: str = './sql/init.sql'     # path to init sql script
 
     redis_host: str = 'localhost'       # redis host
     redis_port: int = 6379              # redis port
