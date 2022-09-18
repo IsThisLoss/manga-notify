809a624c48a2d76e46e5632edda69b0135de1d83
isthisloss <kopturovdim@gmail.com>
2022-09-18 13:35:49 +0000

    fix: cron interval

--- manga_notify/jobs/__init__.py
+++ manga_notify/jobs/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from datetime import datetime
+import typing
+
 import arq
 
 from . import background_parsing
@@ -7,6 +11,16 @@ from . import remind_later
 from .. import dependencies
 
 
+def gen_minutes(interval: int) -> typing.Set[int]:
+    start = datetime.now().minute
+    result = set()
+    for minute in range(start, 60, interval):
+        result.add(minute)
+    for minute in range(start, 0, -interval):
+        result.add(minute)
+    return result
+
+
 async def run():
     deps = dependencies.get()
     cfg = deps.get_cfg()
@@ -14,6 +28,13 @@ async def run():
 
     async def on_startup(ctx):
         ctx['deps'] = deps
+
+    logging.info('Parsing interval is %s mins', cfg.parsing_interval)
+    minutes = gen_minutes(cfg.parsing_interval)
+    logging.info(
+        'Going to run cron at %s minutes of hour',
+        ','.join(map(str, sorted(minutes))),
+    )
 
     worker = arq.worker.Worker(
         functions=[
@@ -28,7 +49,7 @@ async def run():
             arq.cron(
                 background_parsing.job,
                 name='background_parsing',
-                microsecond=cfg.parsing_interval * 60000,
+                minute=minutes,
             )
         ],
         redis_pool=redis,
