56ec22a074cb3f64897696beee74fa551197f335
IsThisLoss <kopturovdim@gmail.com>
2023-04-01 20:48:05 +0400

    feature: send message from cli

--- manga_notify/__main__.py
+++ manga_notify/__main__.py
@@ -7,23 +7,27 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-
 import asyncio
 import argparse
 
 from . import bot
 from . import jobs
+from . import custom
 
 
 async def main():
     parser = argparse.ArgumentParser('Manga notify')
     parser.add_argument('mode', metavar='MODE', help='Run mode: bot or jobs')
+    parser.add_argument('--user-id', nargs='?', help='UserId for send_message mode')
+    parser.add_argument('--message', nargs='?', help='message for send_message mode')
     args = parser.parse_args()
 
     if args.mode == 'bot':
         await bot.dp.start_polling()
     elif args.mode == 'jobs':
         await jobs.run()
+    elif args.mode == 'send_message':
+        await custom.send_message(args.user_id, args.message)
     else:
         logging.fatal(f'Unknown mode: {args.mode}')
         exit(1)
