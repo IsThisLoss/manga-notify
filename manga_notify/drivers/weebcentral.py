b923659bf35631bf20cdb297e2b129553820ece6
Dmitry Kopturov <KopturovDim@gmail.com>
2025-03-09 12:07:10 +0300

    feature: add weebcentral parser (#58)

---
+++ manga_notify/drivers/weebcentral.py
@@ -0,0 +1,11 @@
+from .basic_rss import BasicRss
+
+from ..database import feed_storage
+
+
+class WeebCentralRss(BasicRss):
+    def is_match(self, url: str) -> bool:
+        return 'mangasee' in url
+
+    def feed_type(self) -> str:
+        return feed_storage.FeedType.Manga
